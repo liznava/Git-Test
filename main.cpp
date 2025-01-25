6b185951b616df73758cd9b24c5dd6ecf1292e12
Lizette Navarrete <liznava@ut.utm.edu>
2025-01-24 21:37:48 -0600

    Commiting new sum function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
-  
+  int n = 0;
+  std::cout << "Give a number: ";
+  std::cin >> n;
+  cout << sum(n);
   return 0;
 }
 
+int sum(int n){
+
+  int total = 0;
+  for(int i =1; i <= n; i++)
+    {
+      total +=i;
+    }
+  return total;
+}
 
